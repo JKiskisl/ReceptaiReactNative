1a42365daef4e72d725bf935ccfa8cc958bd0423
JKiskisl <smicscl@gmail.com>
2022-09-17 17:25:35 +0300

    Sriubos

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Receptai from './Receptai.json';
 
-function TradiciniaiScreen({navigation}) {
+
+function VegetariskiScreen({navigation}) {
+  return(
+    <View style={{flex:1,alignItems:'center', justifyContent:'center'}}>
+      <Button title="Go to home" onPress={()=>navigation.navigate('Home')}/>
+      <Button title="Go back" onPress={()=> navigation.goBack()}/>
+    </View>
+  )
+}
+
+function MesosScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button title="Go to home" onPress={()=> navigation.navigate('Home')}/>
+      <Button title="Go back" onPress={()=> navigation.goBack()}/>
+    </View>
+  )
+}
+
+function SriubosScreen({navigation}) {
+
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Tradiciniai receptai</Text>
+      <View style={styles.container}>
+        {Receptai.Sriuba.map((item)=>(
+          <View>
+            <Button title={item.name} onPress={()=>{Alert.alert(item.paruosimas)}}/>
+          </View>
+        ))}
+
+      </View>
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
@@ -41,10 +70,20 @@ function DetailsScreen({ route, navigation }) {
       <Text>itemId: {JSON.stringify(itemId)}</Text>
       <Text>otherParam: {JSON.stringify(otherParam)}</Text>
       <Button
-       title="Tradciniai receptai"
+       title="Sriubos"
        onPress={() => {
-         navigation.navigate('Tradiciniai Receptai')
+         navigation.navigate('Sriubos')
        }}/>
+       <Button 
+        title="Mesos receptai"
+        onPress={()=>{
+          navigation.navigate('Mesa')
+        }}/>
+        <Button
+        title="Vegetariski receptai"
+        onPress={()=>{
+          navigation.navigate('Vegetariski')
+        }}/>
       <Button
         title="Go to Details... again"
         onPress={() =>
@@ -67,7 +106,9 @@ function App() {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
-        <Stack.Screen name="Tradiciniai Receptai" component={TradiciniaiScreen} />
+        <Stack.Screen name="Sriubos" component={SriubosScreen} />
+        <Stack.Screen name="Mesa" component={MesosScreen} />
+        <Stack.Screen name="Vegetariski" component={VegetariskiScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
